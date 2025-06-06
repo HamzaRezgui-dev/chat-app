24350f09eeb36774d5baf9672f8f726fa461b47b
ChazzitUP <hamzarezgui.education@gmail.com>
2025-06-06 19:16:55 +0100

    implemented the design of the login and register page

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,8 +1,59 @@
 import React from "react";
 import "./Login.css";
+import assets from "../../assets/assets";
 
 const Login = () => {
-  return <div>Login</div>;
+  const [currState, setCurrState] = React.useState<string>("Sign up");
+  return (
+    <div className="login">
+      <img src={assets.logo_big} alt="" className="logo" />
+      <form className="login-form">
+        <h2>{currState}</h2>
+        {currState === "Sign up" && (
+          <input
+            type="text"
+            placeholder="username"
+            className="form-input"
+            required
+          />
+        )}
+        <input
+          type="email"
+          placeholder="email address"
+          className="form-input"
+          required
+        />
+        <input
+          type="password"
+          placeholder="password"
+          className="form-input"
+          required
+        />
+        <button type="submit">
+          {currState === "Sign up" ? "Sign up" : "Login Now"}
+        </button>
+        <div className="login-term">
+          <input type="checkbox" name="" id="" />
+          <p>Agree to the terms of use & privacy policy.</p>
+        </div>
+        <div className="login-forgot">
+          <p className="login-toggle">
+            {currState === "Sign up" ? (
+              <>
+                Already have an account?{" "}
+                <span onClick={() => setCurrState("Login")}>Login here</span>
+              </>
+            ) : (
+              <>
+                Don't have an account?{" "}
+                <span onClick={() => setCurrState("Sign up")}>Create one</span>
+              </>
+            )}
+          </p>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
